052075cbd2c2f4d0cc6148c035154dc57513f3b8
Tom Deakin <tom.deakin@bristol.ac.uk>
2022-03-10 12:35:18 +0000

    allocate arrays for velocity and pressue

--- include/flow/FlowSolver.hpp
+++ include/flow/FlowSolver.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "utils/utils.hpp"
+
 namespace minicombust::flow 
 {
     template<class T>
@@ -10,7 +12,62 @@ namespace minicombust::flow
             T combustion_field;
             T flow_field;
 
+            Mesh<T> *global_mesh;
+
+            // Velocity (cell centered)
+            T* velocity_U;
+            T* velocity_V;
+            T* velocity_W;
+
+            // Velocity gradients
+            vec<T>* velocity_dUdX;
+            vec<T>* velocity_dVdX;
+            vec<T>* velocity_dWdX;
+
+            // Pressure (cell centered)
+            T* pressure;
+            vec<T>* pressure_dPdX;
+
         public:
+
+            FlowSolver(Mesh<T> *global_mesh) : global_mesh(global_mesh) {
+
+                // Allocate velocity fields + gradients
+                velocity_U = (T*)malloc(sizeof(T) * global_mesh->mesh_size);
+                velocity_V = (T*)malloc(sizeof(T) * global_mesh->mesh_size);
+                velocity_W = (T*)malloc(sizeof(T) * global_mesh->mesh_size);
+
+                velocity_dUdX = (vec<T>*)malloc(sizeof(vec<T>) * global_mesh->mesh_size);
+                velocity_dVdX = (vec<T>*)malloc(sizeof(vec<T>) * global_mesh->mesh_size);
+                velocity_dWdX = (vec<T>*)malloc(sizeof(vec<T>) * global_mesh->mesh_size);
+
+                printf("Allocating velocities + gradients, %.2f MB\n", (float)((sizeof(vec<T>) + sizeof(T)) * global_mesh->mesh_size * 3) / 1000000.0);
+
+                // Initalize to default value
+                const T initial_velocity = 0.7;
+                for (auto i = 0; i < global_mesh->mesh_size; ++i) {
+                    velocity_U[i] = initial_velocity;
+                    velocity_V[i] = initial_velocity;
+                    velocity_W[i] = initial_velocity;
+
+                    velocity_dUdX[i] = {0.0, 0.0, 0.0};
+                    velocity_dVdX[i] = {0.0, 0.0, 0.0};
+                    velocity_dWdX[i] = {0.0, 0.0, 0.0};
+                }
+
+                // Allocate pressure
+                pressure = (T*)malloc(sizeof(T) * global_mesh->mesh_size);
+                pressure_dPdX = (vec<T>*)malloc(sizeof(vec<T>) * global_mesh->mesh_size);
+                printf("Allocating pressure + gradient, %.2f MB\n", (float)((sizeof(vec<T>)+sizeof(T)) * global_mesh->mesh_size) / 1000000.0);
+
+                // Initalize to default value
+                const T initial_pressure = 0.35;
+                for (auto i = 0; i < global_mesh->mesh_size; ++i) {
+                    pressure[i] = initial_pressure;
+                    pressure_dPdX[i] = {0.0, 0.0, 0.0};
+                }
+
+            }
 
             void update_flow_field();  // Synchronize point with flow solver
             
