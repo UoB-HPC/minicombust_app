7518e3d52c2ae50a62d576b3662b9b2d72151c71
Harry Waugh <hwaugh@eos0384.eos.clusters.nvidia.com>
2024-10-12 10:28:32 -0700

    Updated README and added job params

--- build_devtech.sh
+++
@@ -1,3 +0,0 @@
-export LD_LIBRARY_PATH=/lustre/fsw/coreai_devtech_all/hwaugh/repos/AMGX_vector_upload/install/lib/:$LD_LIBRARY_PATH
-make CUDA_INSTALL_PATH=/usr/local/cuda/ AMGX_INSTALL_PATH=/lustre/fsw/coreai_devtech_all/hwaugh/repos//AMGX_vector_upload/install/ MPI_INSTALL_PATH=/usr/local/openmpi/ clean gpu
-#make CUDA_INSTALL_PATH=/opt/nvidia/hpc_sdk/Linux_x86_64/24.3/cuda/ AMGX_INSTALL_PATH=/lustre/fsw/coreai_devtech_all/hwaugh/repos//AMGX/install/ MPI_INSTALL_PATH=/lustre/fsw/coreai_devtech_all/hwaugh/repos/mpi/ clean gpu
